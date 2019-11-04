ff6cc4abdb6435090a10c042f5db4526c1e8e969
Luca Koval <lucakoval@me.com>
2019-11-04 17:37:00 -0500

    Finishing indices.ml

---
+++ _build/player.ml
@@ -0,0 +1,70 @@
+open Yojson.Basic.Util
+
+type player = {
+  id: int;
+  score: int;
+  location: int;
+  properties: string list
+}
+
+type players = {
+  player_list : player list;
+  current_player : int;
+  number_of_players : int;
+  player_names : string list
+}
+
+(** creates the original player data structure for each player *)
+let to_player empty_list = {
+  id = 0;
+  score = 0;
+  location = 0;
+  properties = []
+}
+
+(** creates the original players data structure using user input*)
+let to_players num_players player_names = (*{
+                                            player_list = List.map to_player;
+                                            current_player = 0;
+                                            number_of_players = user_input
+                                            }*)failwith "unimplemented"
+
+(** takes in a 0 just for shits and returns a number rolled by 2 dice*)
+let dice zero =
+  (Random.int 6) + (Random.int 6) +2 +zero
+
+(** [new_property player] is Some property that the current player obtains if any, otherwise None*)
+let new_property player =
+  failwith "unimplemented"
+
+(** updates the current player's state if its their turn (ex: location, score, potential property changes) and changes to the next player*)
+let update_current_player player current_player_id =
+  if player.id = current_player_id then {
+    id= player.id;
+    score = player.score;
+    location = player.location + (dice 0);
+    properties = ((new_property player)::(player.properties)) }
+  else {
+    id = player.id;
+    score = player.score;
+    location= player.location;
+    properties = player.properties
+  }
+
+(** makes a list of the current_player id to pass into mapping function in update_players*)
+let rec make_current_id_list players acc =
+  if (players.number_of_players =(List.length acc))
+  then acc
+  else make_current_id_list players ((players.number_of_players )::acc)
+
+(** updates the players state based on their turn (ex: location, score, potential property changes) and changes to the next player*)
+let update_players players =
+  List.map2 update_current_player (players.player_list) (make_current_id_list players [])
+
+(** updates the players state based on their turn (ex: location, score, potential property changes) and changes to the next player*)
+let new_player players = {
+  player_list = update_players players;
+  current_player = players.current_player +1;
+  number_of_players = players.number_of_players
+}
+
--- indices.ml
+++ indices.ml
@@ -1,17 +1,44 @@
-let rec find_loop i = function
-  | [] -> None
-  | h::t -> if h.location = i then Some h else find_loop i t
+open Board
 
-let return_tile i board =
-  let props = board.properties in
-  let prop_found = find_loop i props in
+type tile_object = PropertyTile of Board.property_tile
+                 | CardTile of Board.card_tile 
+                 | TaxTile of Board.tax_tile 
+                 | CornerTile of Board.corner_tile
+
+let rec find_property_tile i (lst:Board.property_tile list) =
+  match lst with
+  | [] -> None
+  | h::t -> if h.location = i then Some (PropertyTile h)
+    else find_property_tile i t
+
+let rec find_card_tile i (lst:Board.card_tile list) =
+  match lst with
+  | [] -> None
+  | h::t -> if h.location = i then Some (CardTile h)
+    else find_card_tile i t
+
+let rec find_tax_tile i (lst:Board.tax_tile list) =
+  match lst with
+  | [] -> None
+  | h::t -> if h.location = i then Some (TaxTile h)
+    else find_tax_tile i t
+
+let rec find_corner_tile i (lst:Board.corner_tile list) =
+  match lst with
+  | [] -> None
+  | h::t -> if h.location = i then Some (CornerTile h)
+    else find_corner_tile i t
+
+let return_tile i (board:Board.t) =
+  let props = board.property_tiles in
+  let prop_found = find_property_tile i props in
   if prop_found <> None then prop_found else
     let card_tiles = board.card_tiles in
-    let card_found = find_loop i card_tiles in
+    let card_found = find_card_tile i card_tiles in
     if card_found <> None then card_found else
-      let taxes = board.taxes in
-      let tax_found = find_loop i taxes in
+      let taxes = board.tax_tiles in
+      let tax_found = find_tax_tile i taxes in
       if card_found <> None then tax_found else
-        let corners = board.corners in
-        let corner_found = find_loop i corners in
+        let corners = board.corner_tiles in
+        let corner_found = find_corner_tile i corners in
         if corner_found <> None then corner_found else None
