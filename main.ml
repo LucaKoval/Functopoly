f63b322d70ec620e7693892a4d551a9988c8bed0
Luca Koval <lucakoval@me.com>
2019-12-11 22:32:13 -0500

    Finishing auctions

--- main.ml
+++ main.ml
@@ -414,11 +414,41 @@ let rec play_game_recursively prev_cmd str_command player_info board =
                      (Board.buy_update_board board 
                         update_player_buy.current_player 
                         prop_name))
-    | No -> (print_string "> ";
-             match read_line () with
-             | exception End_of_file -> exit 0
-             | str -> play_game_recursively str_command str player_info board
-            )
+    | No -> (
+        let current_location = get_current_location player_info in
+        if (is_property (get_property current_location board)) then
+          let current_player = Player.get_current_player player_info in
+          let prop = get_property current_location board in
+          let prop_name = get_property_name prop in
+          print_endline (prop_name ^ " will now be auctioned off.");
+          let auction_info = Auction.auction_prop current_player player_info prop in
+          let unsorted_post_forfeit_player_info = Player.forfeit_player current_player 
+              player_info board auction_info in
+          let post_forfeit_player_info = {
+            unsorted_post_forfeit_player_info with
+            player_list=(List.sort (fun x y -> x.id - y.id) unsorted_post_forfeit_player_info.player_list);
+            current_player=unsorted_post_forfeit_player_info.current_player mod unsorted_post_forfeit_player_info.number_of_players;
+          } in
+          let current_name = (get_current_player_name post_forfeit_player_info) 
+          in
+          print_endline ("Player " ^ current_name ^ ", it's your turn now! Your 
+          current location is "
+                         ^ string_of_int (Player.get_current_location 
+                                            post_forfeit_player_info));
+          print_endline "";
+          print_string  "> ";
+          match read_line () with
+          | exception End_of_file -> exit 0;
+          | str -> play_game_recursively str_command str 
+                     post_forfeit_player_info board
+        else begin
+          print_string "> ";
+          match read_line () with
+          | exception End_of_file -> exit 0
+          | str -> 
+            play_game_recursively str_command str player_info board
+        end
+      )
     | Upgrade -> let current_player_id = (player_info.current_player) in
       let color_groups = Upgrade.get_color_groups current_player_id board in
       let upgradeable_properties = Upgrade.get_upgradeable_properties 
